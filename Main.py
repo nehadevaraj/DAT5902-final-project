d292e06e20b492e52e5a941872b9eabb4a8859b4
nehadevaraj <ec22665@qmul.ac.uk>
2024-01-05 22:17:24 +0000

    reading csv

---
+++ Main.py
@@ -0,0 +1,9 @@
+#importing all necessary libraries: 
+import pandas as pd  
+
+#reading csv file: 
+data = 'Madrid Daily Weather 1997-2015.csv' 
+df = pd.read_csv(data) 
+print(df)  
+
+#continue here:
