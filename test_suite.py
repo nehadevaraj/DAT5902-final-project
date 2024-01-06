ca468858e6bc2bceae04d2005adb5198b8d71bce
nehadevaraj <ec22665@qmul.ac.uk>
2024-01-06 22:17:03 +0000

    import os test 4

--- test_suite.py
+++ test_suite.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+import os
 
 '''
 from my_functions import add_numbers
@@ -14,13 +15,16 @@ if __name__ == '__main__':
     unittest.main()
 '''
 from my_functions import read_csv_file
-class TestReadCSVFile(unittest.TestCase):
+class test_read_csv_file(unittest.TestCase):
     def test_read_csv_file(self):
-        test_file_path = 'C:/Users/nehadevaraj/Library/CloudStorage/OneDrive-QueenMary,UniversityofLondon/2nd year/professional software and career practices/DAT5092 final project/Madrid Daily Weather 1997-2015.csv'
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        test_file_path = os.path.join(script_dir, 'Madrid Daily Weather 1997-2015.csv')
 
         result = read_csv_file(test_file_path)
 
         self.assertIsInstance(result, pd.DataFrame)
+
 
 if __name__ == '__main__':
     unittest.main()
